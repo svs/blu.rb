3657d0e527e20c64d18c1b0fbeba6c3896e591b2
svs <svs@ludwig.(none)>
2009-12-10 12:24:58 +0530

    images

--- blu.rb
+++ blu.rb
@@ -7,7 +7,7 @@ require 'rss/maker'
 
 def entries
   @posts = {}
-  (Dir.entries("views/posts/").reject{|p| /[#~]/.match(p) != nil} - [".","..","layout.erb",".git",".gitignore"]).each do |post| 
+  (Dir.entries("views/posts/").reject{|p| /[#~]/.match(p) != nil} - [".","..","layout.erb",".git",".gitignore", "images"]).each do |post| 
     @posts[File.atime("views/posts/#{post}")] = post
   end
   @posts
@@ -17,18 +17,25 @@ get "/" do
   redirect "/index.html"
 end
 
+get "/blog/images/:filename" do
+  filename = "views/posts/images/#{params[:filename]}"
+  raise Sinatra::NotFound unless File.exists?(filename)
+  File.read(filename) 
+end
+
 get "/blog/:title" do
-  @output = RedCloth.new(File.read("views/posts/#{params[:title]}")).to_html
   if params[:title].index(".erb")
     t = params[:title].split(".")
     @title = t[0]
     if t.size == 3
       layout = File.read("views/posts/_#{t[1]}.erb")
     end
-    erb :"posts/#{params[:title].gsub(".erb","")}", :layout => layout
+    @erb = erb File.read("views/posts/#{params[:title]}"), :layout => layout
+    @output = RedCloth.new(@erb).to_html
+    @output
   else
     @title = params[:title]
-    @output
+    File.read("views/posts/#{params[:title]}")
   end
 end
 
@@ -60,12 +67,28 @@ get "/feed" do
   content.to_xml
 end
   
-get '/update_blog' do #manual update
+def update_blog
   `cd views/posts;git pull`
+  `mkdir public/images` unless File.exists?("public/images")
+  `cp -r views/posts/images/* public/images/`
   "blog updated"
 end
 
+get '/update_blog' do #manual update
+  update_blog
+end
+
 post '/update_blog' do # for github post-commit hook
-  `cd views/posts;git pull origin master`
-  "blog updated"
+  update_blog
+end
+
+helpers do 
+  def image_tag(filename, options={})
+    unless options.empty?
+      attrs = []
+      attrs = options.map { |key, value| %(#{key}="#{Rack::Utils.escape_html(value)}") }
+      @options = " #{attrs.sort * ' '}" unless attrs.empty?
+    end
+    "<img src='/images/#{filename}' #{@options}/>"
+  end
 end
